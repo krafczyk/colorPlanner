be2a08761e13fb9454fa3d77bf9e5173222d4151
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-07-29 22:39:29 -0500

    Much advancement, Up to cloning the base layer

--- colorPlanner/colorPlanner.py
+++ colorPlanner/colorPlanner.py
@@ -1,9 +1,11 @@
 from krita import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import sip
 
 from .colorPlannerForm import Ui_colorPlannerForm
 from .colorPlannerMaskSelectForm import Ui_colorPlannerMaskSelectForm
+from .colorPlannerDialogForm import Ui_colorPlannerDialogForm
 
 def ErrorMessage(message, title="", informative_text=""):
     msgBox = QMessageBox()
@@ -27,6 +29,37 @@ class ColorPlannerMaskSelect(QWidget):
         self.Ui.label.setText("{}:".format(self.node.name()))
         self.Ui.maskCheckBox.setChecked(True)
 
+class ColorPlannerDialog(QDialog):
+    def __init__(self, text, parent):
+        # Initialize Parent
+        super().__init__(parent)
+
+        self.Ui = Ui_colorPlannerDialogForm()
+        self.Ui.setupUi(self)
+        self.textLabel = QLabel(text, self)
+        self.Ui.verticalLayout.insertWidget(0, self.textLabel)
+
+    def setText(self, text):
+        self.textLabel.setText(text)
+
+    @staticmethod
+    def dialogAccepted(text):
+        dialog = ColorPlannerDialog(text, None)
+        dialog.setModal(True)
+        res = dialog.exec_()
+        if res == QDialog.Accepted:
+            return True
+        else:
+            return False
+
+def nodeRemoval(node):
+    # Remove children
+    for child in node.childNodes():
+        nodeRemoval(child)
+        node.removeChildNode(child) # Maybe?
+    # Remove self.
+    node.remove()
+
 class ColorPlannerDocker(DockWidget):
     def __init__(self):
         # Initialize Parent
@@ -49,10 +82,42 @@ class ColorPlannerDocker(DockWidget):
 
         # Connect actions
         self.Ui.selectCurrentLayerAsBaseButton.clicked.connect(self.selectBaseLayer)
+        self.Ui.buildMaskCompositorButton.clicked.connect(self.buildMaskCompositor)
 
         # Variables
+        self.clearInternalVariables()
+
+    def clearInternalVariables(self):
         self.baseLayer = None
-        self.mask_dict_idx = {}
+        self.maskSelectors = []
+        # Clear existing mask selection widgets
+        while not self.scroll_layout.isEmpty():
+            self.scroll_layout.removeItem(self.scroll_layout.itemAt(0))
+
+    def canBuildCompositor(self):
+        if self.baseLayer is None:
+            return False
+
+        num_checked = 0
+        for selector in self.maskSelectors:
+            if selector.Ui.maskCheckBox.isChecked():
+                num_checked += 1
+
+        if num_checked == 0:
+            return False
+
+        return True
+
+    @staticmethod
+    def isNodeOnFirstLevel(node):
+        doc = Krita.instance().activeDocument()
+        root = doc.rootNode()
+        found = False
+        for child in root.childNodes():
+            if child == node:
+                found = True
+                break
+        return found
 
     def selectBaseLayer(self):
         # Get active document and layer
@@ -70,6 +135,11 @@ class ColorPlannerDocker(DockWidget):
                          informative_text="Please lock the layer and try again.")
             return
 
+        # Check that the selected layer is on the first 'level'.
+        if not ColorPlannerDocker.isNodeOnFirstLevel(current_node):
+            ErrorMessage("Nodes not on the first level are currently not supported.")
+            return
+
         # Check that selectionmasks are children of the active node.
         selectionmasks = []
         for node in current_node.childNodes():
@@ -82,14 +152,62 @@ class ColorPlannerDocker(DockWidget):
                          informative_text="Please add selection masks to the selected layer and try again.")
             return
 
-        # Clear existing mask selection widgets
-        while not self.scroll_layout.isEmpty():
-            self.scroll_layout.removeItem(self.scroll_layout.itemAt(0))
+        # We have a valid layer, and can start building the compositor.
+        self.clearInternalVariables()
 
+        self.baseLayer = current_node
         # mask selection widgets
         for mask in selectionmasks:
-            self.scroll_layout.addWidget(ColorPlannerMaskSelect(self.Ui.scrollArea.widget(),mask))
+            maskSelector = ColorPlannerMaskSelect(self.Ui.scrollArea.widget(),mask)
+            self.scroll_layout.addWidget(maskSelector)
+            self.maskSelectors.append(maskSelector)
         self.scroll_layout.addItem(QSpacerItem(40,20,QSizePolicy.Minimum, QSizePolicy.Expanding))
+
+    def buildMaskCompositor(self):
+        if not self.canBuildCompositor():
+            ErrorMessage("Can't currently build a compositor!")
+            return
+        
+        if not ColorPlannerDocker.isNodeOnFirstLevel(self.baseLayer):
+            ErrorMessage("Select node wasn't on the 'first level'! Not currently supported!")
+            return
+
+        doc = Krita.instance().activeDocument()
+        first_level = doc.rootNode().childNodes()
+        to_remove = []
+        for node in first_level:
+            if node != self.baseLayer:
+                to_remove.append(node)
+
+        # User Validation for removal of layers
+        if len(to_remove) > 0:
+
+            message = ["First Level Layers which will be deleted:"]
+            for node in to_remove:
+                message.append("{} ({})".format(node.name(), node.type()))
+            message.append("Proceed?")
+
+            full_msg = "\n".join(message)
+
+            if not ColorPlannerDialog.dialogAccepted(full_msg):
+                return
+
+        # Remove said layers
+        for node in to_remove:
+            node.remove()
+            #doc.rootNode().removeChildNode(node)
+
+        # Build 'clean' grayscale version of base layer.
+        baseClone = self.baseLayer.clone()
+        for child in baseClone.childNodes():
+            child.remove()
+            #self.baseClone.removeChildNode(child)
+        baseClone.setLocked(False)
+        doc.rootNode().addChildNode(baseClone, self.baseLayer)
+        #baseClone.setColorSpace('GRAYA', 'U8', 'Gray-D50-elle-V2-srgbtrc.icc')
+        #self.baseLayer.setColorSpace('GRAYA', 'U8', 'Gray-D50-elle-V2-srgbtrc.icc')
+
+        # Add it to the root node for now.
 
     def canvasChanged(self, canvas):
         # Need to implement!!
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DESTDIR=${HOME}/.local/share/krita/pykrita
 
 pyuic5 colorPlanner/colorPlannerForm.ui > colorPlanner/colorPlannerForm.py
 pyuic5 colorPlanner/colorPlannerMaskSelectForm.ui > colorPlanner/colorPlannerMaskSelectForm.py
+pyuic5 colorPlanner/colorPlannerDialogForm.ui > colorPlanner/colorPlannerDialogForm.py
 
 if [ -e ${DESTDIR}/colorPlanner ]; then
     rm -r ${DESTDIR}/colorPlanner
